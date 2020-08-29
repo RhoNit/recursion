9a3f6574c143cc802bfa16b9bb1afc7ac890ff0f
RhoNit <tb.edumax29@gmail.com>
2020-08-29 17:50:24 +0530

    recursively calculate the exponent of a number

--- power-of-a-number.cpp
+++ power-of-a-number.cpp
@@ -12,20 +12,6 @@ int power_recursion(int a, int i) {
 	return ret;
 }
 
-int optimized_approach(int a, int i) {
-	//base case
-	if(i == 0) { return 1; }
-	
-	//recursive case
-	int ans = optimized_approach(a, i/2);
-	ans *= ans;
-
-	if(i%2 == 1)
-		return 2 * ans;
-	else
-		return ans;
-}
-
 int main() {
 	int var, n;
 	
@@ -34,7 +20,7 @@ int main() {
 	cout << "Enter the power of that number: ";
 	cin >> n;
 
-	cout << optimized_approach(var,n);
+	cout << power_recursion(var,n);
 	cout << endl;
 	return 0;
 }
